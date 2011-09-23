b6962e56124ffea558ff938639dda88f67b4c7f2
Anand Avati <avati@gluster.com>
2011-09-23 17:04:39 +0530

    inode: use a 10x wider hash table
    Change-Id: Ia5b4585f5a03b39f9743d32acf39684ccbb2df5c

--- libglusterfs/src/inode.c
+++ libglusterfs/src/inode.c
@@ -1183,7 +1183,7 @@ inode_table_new (size_t lru_limit, xlator_t *xl)
 
         new->lru_limit = lru_limit;
 
-        new->hashsize = 14057; /* TODO: Random Number?? */
+        new->hashsize = 140570; /* TODO: Random Number?? */
 
         /* In case FUSE is initing the inode table. */
         if (lru_limit == 0)
